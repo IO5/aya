5787c879a3ae1d5e1b34b5d6a5f223b0c178e8c6
io <szpaszek@gmail.com>
2017-05-01 01:00:31 +0200

    moving to cmake

---
+++ aya/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/IO/Source/Repos/aya/src/aya/Lexer.cpp" "C:/Users/IO/Source/Repos/aya/aya/CMakeFiles/lib.dir/Lexer.cpp.obj"
+  "C:/Users/IO/Source/Repos/aya/src/aya/Object.cpp" "C:/Users/IO/Source/Repos/aya/aya/CMakeFiles/lib.dir/Object.cpp.obj"
+  "C:/Users/IO/Source/Repos/aya/src/aya/Parser.cpp" "C:/Users/IO/Source/Repos/aya/aya/CMakeFiles/lib.dir/Parser.cpp.obj"
+  "C:/Users/IO/Source/Repos/aya/src/aya/Token.cpp" "C:/Users/IO/Source/Repos/aya/aya/CMakeFiles/lib.dir/Token.cpp.obj"
+  "C:/Users/IO/Source/Repos/aya/src/aya/Trie.cpp" "C:/Users/IO/Source/Repos/aya/aya/CMakeFiles/lib.dir/Trie.cpp.obj"
+  "C:/Users/IO/Source/Repos/aya/src/aya/main.cpp" "C:/Users/IO/Source/Repos/aya/aya/CMakeFiles/lib.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/lib/gsl/include"
+  "C:/lib/gsl/include/gsl"
+  "C:/lib/boost_1_62_0"
+  "src/aya"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
