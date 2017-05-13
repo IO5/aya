5764ff2b622ab7492f196e00f66a327334277f74
io <szpaszek@gmail.com>
2017-05-13 17:44:35 +0200

    trying out boost hana
    Signed-off-by: io <szpaszek@gmail.com>

--- src/sandbox/main.cpp
+++ src/sandbox/main.cpp
@@ -1,168 +1,75 @@
-#include "Value.hpp"
-#include "CFunction.hpp"
-#include "Object.hpp"
 #include "Token.hpp"
 
 #include <iostream>
-#include <memory>
-#include <boost/lexical_cast.hpp>
+#include <boost/hana/type.hpp>
+#include <boost/hana/tuple.hpp>
+#include <boost/hana/string.hpp>
+#include <boost/hana/adjust_if.hpp>
+#include <boost/hana/front.hpp>
+#include <boost/hana/equal.hpp>
 #include <string_view>
-#include <tuple>
-#include <array>
 #include <type_traits>
-#include <optional>
 
 namespace aya {
 
 namespace detail {
 
-template <typename... Tries>
-struct TrieList;
+namespace bh = boost::hana;
 
-template <char_t ch, typename... Children>
-struct Trie {
-    template <typename R, typename F>
-    static constexpr R match(const char* ptr, F&& stopCond) {
-        if (*ptr == ch)
-            return TrieList<Children...>::template match<R>(ptr + 1, std::forward<F>(stopCond));
-        return R{};
-    }
-};
+template <char_t ch>
+using char_ = bh::integral_constant<char_t, ch>;
+template <char_t ch>
+constexpr char_<ch> char_c{};
 
 template <auto result>
-struct TrieResult {
-    template <typename R, typename F>
-    static constexpr R match(const char* ptr, F&& stopCond) {
-        return stopCond(ptr) ? result : R{};
-    }
+using result_ = bh::integral_constant<decltype(result), result>;
+template <auto result>
+constexpr result_<result> result_c{};
+
+template <char_t Ch, auto Result, typename... Children>
+struct Node {
+    static constexpr char_t ch = Ch;
+    static constexpr auto result = Result;
+    static constexpr bh::tuple<Children...> children{};
+
+    template <typename T>
+    constexpr bool operator==(const T&) const { return std::is_same_v<Node, T>; }
+    template <typename T>
+    constexpr bool operator!=(const T&) const { return ! std::is_same_v<Node, T>; }
 };
 
-template <typename Head, typename... Tries>
-struct TrieList<Head, Tries...> {
-    template <typename R, typename F>
-    static constexpr R match(const char* ptr, F&& stopCond) {
-        if (auto result = Head::template match<R>(ptr, std::forward<F>(stopCond)))
-            return result;
-        return TrieList<Tries...>::template match<R>(ptr, std::forward<F>(stopCond));
-    }
-};
-template <>
-struct TrieList<> {
-    template <typename R, typename F>
-    static constexpr R match(const char*, F&&) {
-        return R{};
-    }
-};
-
-template <typename T> struct is_trie_result : std::false_type {};
-template <auto _> struct is_trie_result<TrieResult<_>> : std::true_type {};
-
-template <typename List, const auto& mapping, size_t charsLeft>
-struct add_trie_to_list;
-
-template <typename InputTrie, char_t ch, const auto& mapping, size_t charsLeft>
-struct try_add_to_trie_impl {
-    using type = InputTrie;
-};
-template <typename InputTrie, const auto& mapping, size_t charsLeft>
-struct try_add_to_trie {
-    static constexpr auto& str = std::get<0>(mapping);
-    static constexpr char ch = str[str.size() - charsLeft];
-
-    using type = typename try_add_to_trie_impl<InputTrie, ch, mapping, charsLeft>::type;
-};
-
-template <char ch, typename TrieList>
-struct make_trie_from_list;
-template <char ch, typename... Tries>
-struct make_trie_from_list<ch, TrieList<Tries...>> {
-    using type = Trie<ch, Tries...>;
-};
-
-template <const auto& mapping, size_t charsLeft, typename... Tries>
-struct add_trie_to_list<TrieList<Tries...>, mapping, charsLeft> {
-    static constexpr auto& str = std::get<0>(mapping);
-    static constexpr char ch = str[str.size() - charsLeft];
-
-    using OriginalList = TrieList<Tries...>;
-    // try add to each child
-    using ExtendedList = TrieList<
-            typename try_add_to_trie<Tries, mapping, charsLeft>::type
-            ...
-        >;
-    using NewTrie = typename make_trie_from_list<
-            ch,
-            typename add_trie_to_list<TrieList<>, mapping, charsLeft - 1>::type
-        >::type;
-
-    // if no one matched append new to the list 
-    using type = std::conditional_t<
-            std::is_same<OriginalList, ExtendedList>::value,
-            TrieList<Tries..., NewTrie>,
-            ExtendedList
-        >;
-};
-template <const auto& mapping, typename... Tries>
-struct add_trie_to_list<TrieList<Tries...>, mapping, 0> {
-    static constexpr auto result = std::get<1>(mapping);
-
-    static_assert(! std::disjunction_v<is_trie_result<Tries>...>, "duplicate words inside trie");
-
-    using type = TrieList<Tries..., TrieResult<result>>;
-};
-
-template <const auto& mapping, size_t charsLeft, char ch, typename... Children>
-struct try_add_to_trie_impl<Trie<ch, Children...>, ch, mapping, charsLeft> {
-    using ExtendedChildrenList = typename add_trie_to_list<
-        TrieList<Children...>,
-        mapping,
-        charsLeft - 1
-    >::type;
-
-    using type = typename make_trie_from_list<ch, ExtendedChildrenList>::type;
-};
-
-template <const auto& mappings, int idx>
-constexpr auto StringMapping = mappings[idx];
-
-template <typename List, const auto& mappings, size_t idx>
-struct make_trie_from_strings {
-    static constexpr auto& mapping = StringMapping<mappings, idx>;
-    static constexpr auto& str = std::get<0>(mapping);
-    static constexpr auto result = std::get<1>(mapping);
-
-    using ExtendedList = typename add_trie_to_list<List, mapping, str.size()>::type;
-    using type = typename make_trie_from_strings<ExtendedList, mappings, idx - 1>::type;
-};
-template <typename List, const auto& mappings>
-struct make_trie_from_strings<List, mappings, 0> {
-    static constexpr auto& mapping = StringMapping<mappings, 0>;
-    static constexpr auto& str = std::get<0>(mapping);
-
-    using type = typename add_trie_to_list<List, mapping, str.size()>::type;
-};
-
+template <char_t ch, auto result, typename... Children>
+constexpr auto makeNode(char_<ch>, result_<result>, bh::tuple<Children...>) {
+    return Node<ch, result, Children...>{};
 }
 
-template <const auto& mappings>
-class Trie {
-    using type = typename detail::make_trie_from_strings<
-        detail::TrieList<>,
-        mappings,
-        sizeof(mappings)/sizeof(mappings[0]) - 1 // last index
-        >::type;
+template <typename Node, typename String, typename Result>
+constexpr auto addStringToNode(Node node, String string, Result result) {
+    auto extendedChildren = addStringToList(node.children, string, result);
 
-    using ResultType = std::remove_reference_t<decltype(std::get<1>(mappings[0]))>;
+    return makeNode(char_c<node.ch>, result_c<node.result>, extendedChildren);
+}
 
-public:
-    template <typename F>
-    static constexpr ResultType match(const char* ptr, F&& stopCond) {
-        return type::template match<ResultType>(ptr, std::forward<F>(stopCond));
+template <typename String, typename Result, typename... Nodes>
+constexpr auto addStringToList(bh::tuple<Nodes...> list, String string, Result result) {
+    if constexpr (bh::is_empty(string)) {
+        return list;
+    } else {
+        auto chMatches = [=](auto trie) { 
+            return bh::front(string) == trie.ch;
+        };
+        auto extendedList = bh::adjust_if(list, chMatches, [](auto trie) {
+            return trie;
+        });
+        if (extendedList == list) {
+            return list;
+        } else {
+            return list;
+        }
     }
-    static constexpr ResultType match(const char* ptr) {
-        return match(ptr, [](auto* ptr){ return !*ptr; });
-    }
-};
+}
+
+}
 
 using namespace std::literals::string_view_literals;
 
@@ -197,12 +104,11 @@ constexpr std::pair<const std::string_view, int> keywords[] = {
 using namespace std;
 using namespace aya;
 
+namespace bh = boost::hana;
 
 int main(int argc, const char* argv[]) {
-    if (argc != 2)
-        return -1;
-
-    auto result = Trie<keywords>::match(argv[1]);
-    cout << result << '\n';
+    (void)argc; (void)argv;
+    constexpr auto res = detail::addStringToList(bh::make_tuple(detail::Node<'e', 23>{}), BOOST_HANA_STRING("abc"), bh::int_c<23>);
+    detail::result_c<23ul>;
     return 0;
 }
